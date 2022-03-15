635d2e2691f0a0e40653a40064ac32b696f52091
Thomas Lux <thomas.ch.lux@gmail.com>
2022-03-15 07:19:57 -0700

    Verifying gradient computation correctness with Jax.

--- research/2022-03_nn_svd/simple_relu_mlp.py
+++ research/2022-03_nn_svd/simple_relu_mlp.py
@@ -1,14 +1,26 @@
-import numpy as np
+# import numpy as np
+import jax.numpy as np
+from jax import grad
+
+try:
+    from jax import random
+    key = random.PRNGKey(0)
+    def uniform(size):
+        return random.uniform(key, size)
+except:
+    np.random.seed(0)
+    def uniform(size):
+        return np.random.uniform(size=size)
+
 
 # Create weight matrices and shift vectors for new model.
-def new_model(state_dims, seed=0):
-    np.random.seed(seed)
+def new_model(state_dims):
     weights = [
-        np.random.uniform(size=(dim_in, dim_out)) for (dim_in, dim_out)
+        uniform(size=(dim_in, dim_out)) for (dim_in, dim_out)
         in zip(state_dims[:-1], state_dims[1:])
     ]
     shifts = [
-        np.random.uniform(size=(dim,)) for dim in state_dims[1:-1]
+        uniform(size=(dim,)) for dim in state_dims[1:-1]
     ]
     return weights, shifts
 
@@ -28,7 +40,7 @@ def gradient(x, y, weights, shifts):
     states = [None] * len(weights)
     output = evaluate(x, weights, shifts, states=states)
     # Compute the squared error gradient.
-    grad = (output - y)
+    grad = (output - y) / x.shape[0]
     # Store the mean squared error.
     mean_squared_error = np.mean(grad**2)
     # Compute the gradient for all weights and shifts.
@@ -37,7 +49,7 @@ def gradient(x, y, weights, shifts):
     weights_grad[-1] = np.matmul(states[-1].T, grad)
     for i in range(len(weights)-2, -1, -1):
         grad = np.where(states[i+1] > 0, np.matmul(grad, weights[i+1].T), 0.0)
-        shifts_grad[i] = np.mean(grad, axis=0)
+        shifts_grad[i] = np.sum(grad, axis=0)
         weights_grad[i] = np.matmul(states[i].T, grad)
     # Return the gradient.
     return weights_grad, shifts_grad, mean_squared_error
@@ -45,6 +57,26 @@ def gradient(x, y, weights, shifts):
 # Take one mean squared error gradient step for a model.
 def step(x, y, weights, shifts, step_size=0.0001):
     weights_grad, shifts_grad, mse = gradient(x, y, weights, shifts)
+    print("Weights grad")
+    for wg in weights_grad:
+        print(wg)
+    print()
+    print("Shifts grad")
+    for sg in shifts_grad:
+        print(sg)
+    print()
+    eval_error = lambda weights, shifts: ((evaluate(x, weights, shifts) - y)**2).mean() / 2
+    eval_grad = grad(eval_error, argnums=[0,1])
+    weights_grad, shifts_grad = eval_grad(weights, shifts)
+    print("True weights grad")
+    for wg in weights_grad:
+        print(wg)
+    print()
+    print("True Shifts grad")
+    for sg in shifts_grad:
+        print(sg)
+    print()
+    exit()
     for i in range(len(weights)):
         weights[i] -= step_size * weights_grad[i]
     for i in range(len(shifts)):
@@ -52,12 +84,14 @@ def step(x, y, weights, shifts, step_size=0.0001):
     return mse
 
 
+
+
 # Configure the model.
 input_dim = 2
 output_dim = 1
-state_dim = 100
-num_states = 1
-num_steps = 10001
+state_dim = 3
+num_states = 3
+num_steps = 1
 num_printouts = 31
 
 # Initialize a model.
@@ -66,7 +100,7 @@ weights, shifts = new_model(state_dims)
 
 # Define some test data.
 n = 200
-x = (np.random.uniform(size=(n, input_dim)) - 0.5)
+x = (uniform(size=(n, input_dim)) - 0.5)
 y = 1 - np.cos(5*np.linalg.norm(x, axis=1, keepdims=True))
 
 # Set the print interval.
